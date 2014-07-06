539c789fc166f2aa0ffe3459f9f17698d4156fbb
Dario Stojanovski <dariostojanovski@Darios-Mac.local>
2014-07-06 20:52:41 +0200

    initial project set

---
+++ TechNews/NewsArticle.h
@@ -0,0 +1,20 @@
+//
+//  NewsArticle.h
+//  TechNews
+//
+//  Created by Dario Stojanovski on 7/6/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewsArticle : NSObject
+
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSString *link;
+@property (nonatomic, strong) NSString *author;
+@property (nonatomic, strong) NSDate *publishDate;
+@property (nonatomic, strong) NSString *imageUrl;
+@property (nonatomic, strong) NSString *sourceName;
+
+@end
---
+++ TechNews/NewsArticle.m
@@ -0,0 +1,13 @@
+//
+//  NewsArticle.m
+//  TechNews
+//
+//  Created by Dario Stojanovski on 7/6/14.
+//
+//
+
+#import "NewsArticle.h"
+
+@implementation NewsArticle
+
+@end
